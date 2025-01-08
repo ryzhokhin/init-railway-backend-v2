8213036126bc5c5ba4f77fe89abcccb93fb3df0e
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-08 15:47:01 -0800

    trying to fix teh stopping container

--- src/index.js
+++ src/index.js
@@ -43,4 +43,5 @@ app.use((req, res, next) => {
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+    console.log("MysqlURL: " + process.env.MYSQL_URL);
 });
