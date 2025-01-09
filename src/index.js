e793c069dfb56fb0cc649c517c1561c6e485b8af
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-08 16:40:36 -0800

    added setup file for tables

--- src/index.js
+++ src/index.js
@@ -28,11 +28,13 @@ app.use((req, res, next) => {
 // routes connections
 const testRoutes = require('./routes/test');
 const setupRoutes = require('./routes/setup');
+const userRoutes = require('./routes/users');
 
 
 // routes application
 app.use('/test', testRoutes);
 app.use('/setup', setupRoutes);
+app.use('/users', userRoutes);
 
 
 app.use((req, res, next) => {
