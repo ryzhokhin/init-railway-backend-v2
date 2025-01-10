f604fbb4dd2762938b05142209583be23cc054fb
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-10 14:49:33 -0800

    large update for users and guides

--- src/index.js
+++ src/index.js
@@ -14,29 +14,19 @@ app.use(cors({
     allowedHeaders: ['Content-Type']
 }));
 
-// testing without headers
-//
-// // Проверка подключения к базе данных
-// app.use((req, res, next) => {
-//     console.log("Request Headers:", req.headers);
-//     res.setHeader('Access-Control-Allow-Origin', 'https://zhiroazhigatel.netlify.app'); // Ваш фронтенд-домен
-//     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Обязательно для cookies/credentials
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); // Поддерживаемые методы
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Обязательно, если передаются данные
-//     next();
-// });
-
 
 // routes connections
 const testRoutes = require('./routes/test');
 const setupRoutes = require('./routes/setup');
 const userRoutes = require('./routes/users');
+const guidesRoutes = require('./routes/guides');
 
 
 // routes application
 app.use('/test', testRoutes);
 app.use('/setup', setupRoutes);
 app.use('/users', userRoutes);
+app.use('/guides', guidesRoutes);
 
 
 app.use((req, res, next) => {
---
+++ src/routes/guides.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../db/connection')
+
+router.get('/all', async (req, res) => {
+   try{
+       const [guides] = await db.query('SELECT * FROM GUIDES_TABLE');
+       if(guides.length === 0){
+           return res.status(404).json({error: 'No guides found'});
+       }
+       res.status(200).json({
+           message: "Guides fetched successfully",
+           guides,
+       });
+   } catch (error) {
+       console.error("Error fetching guids:", error);
+       res.status(500).json({error: 'Server error'});
+   }
+});
+
+module.exports = router;
+
+
+
+
