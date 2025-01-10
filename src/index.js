7c746446fd38380d9de12595e421eddc3f4ea0fe
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-01-10 13:17:07 -0800

    testing updated users

--- src/index.js
+++ src/index.js
@@ -15,16 +15,16 @@ app.use(cors({
 }));
 
 // testing without headers
-
-// Проверка подключения к базе данных
-app.use((req, res, next) => {
-    console.log("Request Headers:", req.headers);
-    res.setHeader('Access-Control-Allow-Origin', 'https://zhiroazhigatel.netlify.app'); // Ваш фронтенд-домен
-    res.setHeader('Access-Control-Allow-Credentials', 'true'); // Обязательно для cookies/credentials
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); // Поддерживаемые методы
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Обязательно, если передаются данные
-    next();
-});
+//
+// // Проверка подключения к базе данных
+// app.use((req, res, next) => {
+//     console.log("Request Headers:", req.headers);
+//     res.setHeader('Access-Control-Allow-Origin', 'https://zhiroazhigatel.netlify.app'); // Ваш фронтенд-домен
+//     res.setHeader('Access-Control-Allow-Credentials', 'true'); // Обязательно для cookies/credentials
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE'); // Поддерживаемые методы
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Обязательно, если передаются данные
+//     next();
+// });
 
 
 // routes connections
