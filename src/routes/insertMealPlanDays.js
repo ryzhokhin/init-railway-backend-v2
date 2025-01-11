c73ae8230019cc4ab39044d85788abbbb25268e1
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-10 16:26:36 -0800

    adding data into MEALPLANS_DAYS_TABLE

--- src/routes/insertMealPlanDays.js
+++ src/routes/insertMealPlanDays.js
@@ -7,13 +7,13 @@ router.post('/insert-sample', async (req, res) => {
         await db.query(`
             INSERT INTO MEALPLANS_DAYS_TABLE (meal_plan_id, day_number, total_kcal, total_protein, total_fat, total_carbs)
             VALUES
-            (1, 1, 1266, 99, 46, 116),
-            (1, 2, 1291, 96, 58, 92),
-            (1, 3, 1321, 100, 69, 65),
-            (1, 4, 1289, 87, 46, 124),
-            (1, 5, 1320, 105, 53, 100),
-            (1, 6, 1237, 83, 53, 101),
-            (1, 7, 1307, 124, 40, 111);
+            (2, 1, 1338, 131, 31, 127),
+            (2, 2, 1332, 96, 46, 130),
+            (2, 3, 1353, 119, 49, 111),
+            (2, 4, 1302, 94, 49, 121),
+            (2, 5, 1400, 125, 58, 90),
+            (2, 6, 1402, 110, 45, 136),
+            (2, 7, 1401, 100, 56, 113);
         `);
 
         res.status(200).json({ message: 'Sample meal plan days inserted successfully!' });
