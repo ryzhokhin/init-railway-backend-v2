1a1f8b4f42310de74145efe4c94a749ff5d9a809
ilyakudrenko <ilya.kudrenko@gmail.com>
2025-01-13 19:09:32 -0800

    deleting MEALS_TABLE

--- src/routes/mealsRoutes.js
+++ src/routes/mealsRoutes.js
@@ -5,22 +5,23 @@ const db = require('../db/connection'); // Подключение к вашей 
 // Маршрут для удаления таблицы MEALS_TABLE
 router.post('/create-meals-table', async (req, res) => {
     try {
-        const query = `
+
+        await db.query(`
             CREATE TABLE IF NOT EXISTS MEALS_TABLE (
                 id INT AUTO_INCREMENT PRIMARY KEY,
                 meal_plan_day_id INT NOT NULL,
-                type ENUM('завтрак', 'обед', 'ужин', 'перекус') NOT NULL,
-                composition TEXT NOT NULL,
+                type ENUM('завтрак', 'обед', 'ужин', 'перекус'),
+                composition TEXT,
                 preparation TEXT,
                 image_src TEXT,
                 kcal INT,
                 protein INT,
                 fat INT,
                 carbs INT,
-                FOREIGN KEY (meal_plan_day_id) REFERENCES MEALPLANDAYS_TABLE(id) ON DELETE CASCADE
-            )
-        `;
-        await db.query(query);
+                FOREIGN KEY (meal_plan_day_id) REFERENCES MEALPLANS_DAYS_TABLE(id)
+            );
+        `);
+
         res.status(200).json({ message: 'MEALS_TABLE успешно создана.' });
     } catch (error) {
         console.error('Ошибка при создании таблицы:', error); // Логирование ошибки
