6e5f1598951d02520f1fe850d888940e06095a24
DanyloRyzhokhin <dryzhokhin@student-bmhs-la.org>
2025-02-04 01:20:28 -0800

    session management

--- src/index.js
+++ src/index.js
@@ -41,7 +41,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24, // Session expires after 24 hours
         // secure: process.env.NODE_ENV === 'production', // Enable only in production (HTTPS)
         httpOnly: true,
-        sameSite: 'none', // Required for cross-origin cookies
+        sameSite: 'None', // Required for cross-origin cookies
     }
 }));
 
